8ebe8d28e479df52aaa8e60c4d5225a46636d682
Daniel Kersten <dkersten1994@gmail.com>
2020-03-18 17:51:19 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,14 @@
 # Code your solution here!
+
+generate_random_number
+  random_number =  rand(6) + 1
+  return random_number
+end
+
+get_number_input
+  guessed_number = gets.chomp
+end
+
+run_guessing_game
+  
+end
